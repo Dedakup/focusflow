f062231ba55e0b5a52f58684c75ee8d1410819d0
umesh <umesharma.us11@gmail.com>
2024-08-06 18:38:52 +0530

    Added functionality to select current tasks from task list

--- src/components/PomodoroTimer.jsx
+++ src/components/PomodoroTimer.jsx
@@ -4,182 +4,204 @@ import SettingsPopover from './SettingsPopover';
 import TaskPopover from './TaskPopover.jsx';
 
 const PomodoroTimer = () => {
-    const initialRadius = 115;
-    const expandedRadius = 175;
-    const defaultPomodoroTime = 1500; // 25 minutes in seconds
-    const defaultShortBreakTime = 300; // 5 minutes in seconds
-    const defaultLongBreakTime = 900; // 15 minutes in seconds
+  const initialRadius = 115;
+  const expandedRadius = 175;
+  const defaultPomodoroTime = 1500; // 25 minutes in seconds
+  const defaultShortBreakTime = 300; // 5 minutes in seconds
+  const defaultLongBreakTime = 900; // 15 minutes in seconds
 
-    const [radius, setRadius] = useState(initialRadius);
-    const [timeLeft, setTimeLeft] = useState(defaultPomodoroTime);
-    const [isActive, setIsActive] = useState(false);
-    const [cycle, setCycle] = useState(1);
-    const [phase, setPhase] = useState('work'); // work, shortBreak, longBreak
-    const [hasStarted, setHasStarted] = useState(false);
-    const [pomodoroTime, setPomodoroTime] = useState(25); // in minutes
-    const [shortBreakTime, setShortBreakTime] = useState(5); // in minutes
-    const [longBreakTime, setLongBreakTime] = useState(15); // in minutes
+  const [radius, setRadius] = useState(initialRadius);
+  const [timeLeft, setTimeLeft] = useState(defaultPomodoroTime);
+  const [isActive, setIsActive] = useState(false);
+  const [cycle, setCycle] = useState(1);
+  const [phase, setPhase] = useState('work'); // work, shortBreak, longBreak
+  const [hasStarted, setHasStarted] = useState(false);
+  const [pomodoroTime, setPomodoroTime] = useState(25); // in minutes
+  const [shortBreakTime, setShortBreakTime] = useState(5); // in minutes
+  const [longBreakTime, setLongBreakTime] = useState(15); // in minutes
 
-    const [isVisible, setIsVisible] = useState(true);
-    const [tasks, setTasks] = useState([
-        { id: 1, name: 'Task 1', completed: false },
-        { id: 2, name: 'Task 2', completed: false },
-        { id: 3, name: 'Task 3', completed: false },
-        { id: 4, name: 'Task 4', completed: false },
-        { id: 5, name: 'Task 5', completed: false },
-    ]);
-    const [newTask, setNewTask] = useState('');
+  const [isVisible, setIsVisible] = useState(true);
+  const [tasks, setTasks] = useState([
+    { id: 1, name: 'Task 1', completed: false, selected: false },
+    { id: 2, name: 'Task 2', completed: false, selected: true },
+    { id: 3, name: 'Task 3', completed: false, selected: false },
+    { id: 4, name: 'Task 4', completed: false, selected: false },
+    { id: 5, name: 'Task 5', completed: false, selected: false },
+  ]);
+  const [newTask, setNewTask] = useState('');
 
-    useEffect(() => {
-        let interval;
-        if (isActive) {
-            interval = setInterval(() => {
-                setTimeLeft((prevTime) => {
-                    if (prevTime > 0) {
-                        return prevTime - 1;
-                    } else {
-                        clearInterval(interval);
-                        handlePhaseTransition();
-                        return prevTime;
-                    }
-                });
-            }, 1000);
-        } else if (!isActive && timeLeft !== 0) {
+  useEffect(() => {
+    let interval;
+    if (isActive) {
+      interval = setInterval(() => {
+        setTimeLeft((prevTime) => {
+          if (prevTime > 0) {
+            return prevTime - 1;
+          } else {
             clearInterval(interval);
-        }
-        return () => clearInterval(interval);
-    }, [isActive, timeLeft]);
+            handlePhaseTransition();
+            return prevTime;
+          }
+        });
+      }, 1000);
+    } else if (!isActive && timeLeft !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, timeLeft]);
 
-    const handlePhaseTransition = () => {
-        if (phase === 'work') {
-            if (cycle < 4) {
-                setPhase('shortBreak');
-                setTimeLeft(shortBreakTime * 60); // Convert to seconds
-                setCycle(cycle + 1);
-            } else {
-                setPhase('longBreak');
-                setTimeLeft(longBreakTime * 60); // Convert to seconds
-                setCycle(1);
-            }
-        } else {
-            setPhase('work');
-            setTimeLeft(pomodoroTime * 60); // Convert to seconds
-        }
-        setIsActive(true);
-    };
-
-    const toggleTimer = () => {
-        if (!hasStarted) setHasStarted(true);
-        setIsActive(!isActive);
-        setRadius(isActive ? initialRadius : expandedRadius);
-    };
-
-    const resetTimer = (event) => {
-        event.stopPropagation(); // Prevent the timer from starting
-        setTimeLeft(pomodoroTime * 60);
-        setIsActive(false);
-        setPhase('work');
+  const handlePhaseTransition = () => {
+    if (phase === 'work') {
+      if (cycle < 4) {
+        setPhase('shortBreak');
+        setTimeLeft(shortBreakTime * 60); // Convert to seconds
+        setCycle(cycle + 1);
+      } else {
+        setPhase('longBreak');
+        setTimeLeft(longBreakTime * 60); // Convert to seconds
         setCycle(1);
-        setHasStarted(false);
-        setRadius(initialRadius); // Reset radius to initial
-    };
+      }
+    } else {
+      setPhase('work');
+      setTimeLeft(pomodoroTime * 60); // Convert to seconds
+    }
+    setIsActive(true);
+  };
 
-    useEffect(() => {
-        if (isActive) {
-            // Start hiding animation
-            const timer = setTimeout(() => {
-                setIsVisible(false); // Hide element after animation completes
-            }, 500); // Animation duration 500ms
-            return () => clearTimeout(timer);
-        } else {
-            // Show element immediately before animation starts
-            setIsVisible(true);
-        }
-    }, [isActive]);
+  const toggleTimer = () => {
+    if (!hasStarted) setHasStarted(true);
+    setIsActive(!isActive);
+    setRadius(isActive ? initialRadius : expandedRadius);
+  };
 
-    const handlePomodoroChange = (e) => {
-        setPomodoroTime(e.target.value);
-        if (phase === 'work') {
-            setTimeLeft(e.target.value * 60); // Convert to seconds
-        }
-    };
+  const resetTimer = (event) => {
+    event.stopPropagation(); // Prevent the timer from starting
+    setTimeLeft(pomodoroTime * 60);
+    setIsActive(false);
+    setPhase('work');
+    setCycle(1);
+    setHasStarted(false);
+    setRadius(initialRadius); // Reset radius to initial
+  };
 
-    const handleShortBreakChange = (e) => {
-        setShortBreakTime(e.target.value);
-    };
+  useEffect(() => {
+    if (isActive) {
+      // Start hiding animation
+      const timer = setTimeout(() => {
+        setIsVisible(false); // Hide element after animation completes
+      }, 500); // Animation duration 500ms
+      return () => clearTimeout(timer);
+    } else {
+      // Show element immediately before animation starts
+      setIsVisible(true);
+    }
+  }, [isActive]);
 
-    const handleLongBreakChange = (e) => {
-        setLongBreakTime(e.target.value);
-    };
+  const handlePomodoroChange = (e) => {
+    setPomodoroTime(e.target.value);
+    if (phase === 'work') {
+      setTimeLeft(e.target.value * 60); // Convert to seconds
+    }
+  };
 
-    const addTask = () => {
-        if (newTask.trim() !== '') {
-            setTasks([...tasks, { id: Date.now(), name: newTask, completed: false }]);
-            setNewTask('');
-        }
-    };
+  const handleShortBreakChange = (e) => {
+    setShortBreakTime(e.target.value);
+  };
 
-    const deleteTask = (id) => {
-        setTasks(tasks.filter(task => task.id !== id));
-    };
+  const handleLongBreakChange = (e) => {
+    setLongBreakTime(e.target.value);
+  };
 
-    const toggleCompleteTask = (id) => {
-        setTasks(tasks.map(task => task.id === id ? { ...task, completed: !task.completed } : task));
-    };
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, { id: Date.now(), name: newTask, completed: false }]);
+      setNewTask('');
+    }
+  };
 
-    const handleEditTask = (id) => {
-        // Implement edit task functionality here
-        console.log(`Edit task ${id}`);
-    };
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
-    return (
-        <div className="absolute inset-0 flex flex-col items-center justify-center z-50">
-            <div className="flex items-center">
-                {/* Task Popover */}
-                <TaskPopover
-                    tasks={tasks}
-                    newTask={newTask}
-                    setNewTask={setNewTask}
-                    addTask={addTask}
-                    deleteTask={deleteTask}
-                    toggleCompleteTask={toggleCompleteTask}
-                    handleEditTask={handleEditTask}
-                    isVisible={isVisible}
-                    isActive={isActive}
-                />
-                {/* Timer Circle */}
-                <TimerCircle
-                    radius={radius}
-                    timeLeft={timeLeft}
-                    isActive={isActive}
-                    toggleTimer={toggleTimer}
-                    resetTimer={resetTimer}
-                    phase={phase}
-                    hasStarted={hasStarted}
-                />
-                {/* Settings Popover */}
-                <SettingsPopover
-                    isVisible={isVisible}
-                    isActive={isActive}
-                    handlePomodoroChange={handlePomodoroChange}
-                    handleShortBreakChange={handleShortBreakChange}
-                    handleLongBreakChange={handleLongBreakChange}
-                    pomodoroTime={pomodoroTime}
-                    shortBreakTime={shortBreakTime}
-                    longBreakTime={longBreakTime}
-                />
-            </div>
-            {/* Current Task Section */}
-            <span className='text-lg text-white mt-[32px]'>{'Current task'}</span>
-            {
-                phase === 'work' && (
-                    <span className="text-sm text-white mt-[8px]">
-                        {'Breaks until long break: ' + (4 - cycle)}
-                    </span>
-                )
-            }
-        </div >
+  const toggleCompleteTask = (id) => {
+    setTasks(
+      tasks.map(
+        (task) => task.id === id && { ...task, completed: !task.completed }
+      )
     );
+  };
+
+  const handleEditTask = (id) => {
+    // Implement edit task functionality here
+    console.log(`Edit task ${id}`);
+  };
+  const handleSelectedTask = (id) => {
+    const selectedTask = tasks.map((task) =>
+      task.id === id
+        ? { ...task, selected: true }
+        : { ...task, selected: false }
+    );
+
+    setTasks(selectedTask);
+  };
+  return (
+    <div className="absolute inset-0 flex flex-col items-center justify-center z-50">
+      <div className="flex items-center">
+        {/* Task Popover */}
+        <TaskPopover
+          tasks={tasks}
+          newTask={newTask}
+          setNewTask={setNewTask}
+          addTask={addTask}
+          deleteTask={deleteTask}
+          toggleCompleteTask={toggleCompleteTask}
+          handleEditTask={handleEditTask}
+          isVisible={isVisible}
+          isActive={isActive}
+          handleSelectedTask={handleSelectedTask}
+        />
+        {/* Timer Circle */}
+        <TimerCircle
+          radius={radius}
+          timeLeft={timeLeft}
+          isActive={isActive}
+          toggleTimer={toggleTimer}
+          resetTimer={resetTimer}
+          phase={phase}
+          hasStarted={hasStarted}
+        />
+        {/* Settings Popover */}
+        <SettingsPopover
+          isVisible={isVisible}
+          isActive={isActive}
+          handlePomodoroChange={handlePomodoroChange}
+          handleShortBreakChange={handleShortBreakChange}
+          handleLongBreakChange={handleLongBreakChange}
+          pomodoroTime={pomodoroTime}
+          shortBreakTime={shortBreakTime}
+          longBreakTime={longBreakTime}
+        />
+      </div>
+      {/* Current Task Section */}
+      {tasks &&
+        tasks.map(
+          (task) =>
+            task.selected && (
+              <p className="text-lg text-gray-400 mt-[32px]" key={task.id}>
+                <span className="font-semibold text-white capitalize">
+                  Current task :
+                </span>{' '}
+                {task.name}
+              </p>
+            )
+        )}
+      {phase === 'work' && (
+        <span className="text-sm text-white mt-[8px]">
+          {'Breaks until long break: ' + (4 - cycle)}
+        </span>
+      )}
+    </div>
+  );
 };
 
 export default PomodoroTimer;
--- src/components/TaskPopover.jsx
+++ src/components/TaskPopover.jsx
@@ -1,90 +1,163 @@
 import React from 'react';
-import { Popover, PopoverHandler, PopoverContent, IconButton, Input } from '@material-tailwind/react';
+import {
+  Popover,
+  PopoverHandler,
+  PopoverContent,
+  IconButton,
+  Input,
+} from '@material-tailwind/react';
+import CompletedButton from './Buttons/CompletedButton';
 
-const TaskPopover = ({ tasks, newTask, setNewTask, addTask, deleteTask, toggleCompleteTask, handleEditTask, isVisible, isActive }) => {
-    const handlePopoverClick = (event) => {
-        event.stopPropagation();
-        event.preventDefault();
-    };
+const TaskPopover = ({
+  tasks,
+  newTask,
+  setNewTask,
+  addTask,
+  deleteTask,
+  toggleCompleteTask,
+  handleEditTask,
+  isVisible,
+  isActive,
+  handleSelectedTask,
+}) => {
+  const handlePopoverClick = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+  };
 
-    return (
-        <Popover placement="right" offset={{ mainAxis: 10 }}>
-            <PopoverHandler
-                className={`flex rounded-full items-center w-[40px] h-[40px] mr-[32px] transition-opacity duration-500 ${isActive ? 'opacity-0' : 'opacity-100'} ${isVisible ? 'visible' : 'invisible'}`}
-                onClick={handlePopoverClick}
-            >
-                <IconButton onClick={handlePopoverClick} variant="text" className={`rounded-full w-24 h-24 ml-[32px] transition-opacity duration-500 ${isActive ? 'opacity-0' : 'opacity-100'}`}>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="white"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        width="24"
-                        height="24"
-                        className='text-white'
+  return (
+    <Popover placement="right" offset={{ mainAxis: 10 }}>
+      <PopoverHandler
+        className={`flex rounded-full items-center w-[40px] h-[40px] mr-[32px] transition-opacity duration-500 ${
+          isActive ? 'opacity-0' : 'opacity-100'
+        } ${isVisible ? 'visible' : 'invisible'}`}
+        onClick={handlePopoverClick}
+      >
+        <IconButton
+          onClick={handlePopoverClick}
+          variant="text"
+          className={`rounded-full w-24 h-24 ml-[32px] transition-opacity duration-500 ${
+            isActive ? 'opacity-0' : 'opacity-100'
+          }`}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="white"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            width="24"
+            height="24"
+            className="text-white"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+            />
+          </svg>
+        </IconButton>
+      </PopoverHandler>
+      <PopoverContent
+        className={`w-80 h-auto p-4 bg-gray-700 text-white z-50 `}
+      >
+        <div className="flex flex-col space-y-2">
+          <div className="text-sm font-medium text-white">Tasks</div>
+          <div className="text-xs text-gray-400 mb-2">
+            Add, edit, select for session or delete tasks
+          </div>
+          <div className="space-y-2">
+            {tasks &&
+              tasks.map((task) => (
+                <div
+                  key={task.id}
+                  className={`flex items-center justify-between bg-gray-800 p-2 rounded ${
+                    task.selected ? 'bg-green-600' : 'bg-gray-700'
+                  }`}
+                >
+                  <div className="flex items-center space-x-2">
+                    <CompletedButton
+                      id={task.id}
+                      completed={task.completed}
+                      toggleCompleteTask={toggleCompleteTask}
+                    />
+                    <span
+                      className={`cursor-pointer
+                    ${task.completed ? 'line-through text-gray-400' : ''}
+                    `}
+                      onClick={() => handleSelectedTask(task.id)}
                     >
+                      {task.name}
+                    </span>
+                  </div>
+                  <div className="flex items-center space-x-2 z-20">
+                    <button onClick={() => handleEditTask(task.id)}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-5 h-5 text-gray-400 hover:text-white"
+                      >
                         <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M15.232 5.232l3.536 3.536m-3.536-3.536L7.768 13.464a2.25 2.25 0 00-.618 1.414V18h3.121a2.25 2.25 0 001.414-.618l6.464-6.464a2.25 2.25 0 00-3.182-3.182z"
                         />
-                    </svg>
-                </IconButton>
-            </PopoverHandler>
-            <PopoverContent className="w-80 h-auto p-4 bg-gray-700 text-white z-50">
-                <div className="flex flex-col space-y-2">
-                    <div className="text-sm font-medium text-white">Tasks</div>
-                    <div className="text-xs text-gray-400 mb-2">Add, edit, select for session or delete tasks</div>
-                    <div className="space-y-2">
-                        {tasks.map(task => (
-                            <div key={task.id} className="flex items-center justify-between bg-gray-800 p-2 rounded">
-                                <div className="flex items-center space-x-2">
-                                    <button onClick={() => toggleCompleteTask(task.id)}>
-                                        {task.completed ? (
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16" className="w-5 h-5 text-blue-500">
-                                                <path d="M16 2.667L5.833 14 0 8.167l1.666-1.666 4.167 4.167L14.333 1 16 2.667z" />
-                                            </svg>
-                                        ) : (
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-gray-400">
-                                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15m15 0c0-4.142-3.358-7.5-7.5-7.5S4.5 7.858 4.5 12s3.358 7.5 7.5 7.5 7.5-3.358 7.5-7.5zm0 0H8.25" />
-                                            </svg>
-                                        )}
-                                    </button>
-                                    <span className={task.completed ? 'line-through text-gray-400' : ''}>{task.name}</span>
-                                </div>
-                                <div className="flex items-center space-x-2">
-                                    <button onClick={() => handleEditTask(task.id)}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-gray-400 hover:text-white">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.232 5.232l3.536 3.536m-3.536-3.536L7.768 13.464a2.25 2.25 0 00-.618 1.414V18h3.121a2.25 2.25 0 001.414-.618l6.464-6.464a2.25 2.25 0 00-3.182-3.182z" />
-                                        </svg>
-                                    </button>
-                                    <button onClick={() => deleteTask(task.id)}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-gray-400 hover:text-red-500">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 6h-15m3 0v12a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 18V6m-12 0V4.5A1.5 1.5 0 017.5 3h9A1.5 1.5 0 0118 4.5V6m-6 4.5v6m-3-6v6m6-6v6" />
-                                        </svg>
-                                    </button>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                    <div className="flex items-center mt-4">
-                        <Input
-                            value={newTask}
-                            onChange={(e) => setNewTask(e.target.value)}
-                            placeholder="I want..."
-                            className="bg-gray-800 border border-gray-600 rounded-md text-white p-2 w-full"
+                      </svg>
+                    </button>
+                    <button onClick={() => deleteTask(task.id)}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-5 h-5 text-gray-400 hover:text-red-500"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M19.5 6h-15m3 0v12a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 18V6m-12 0V4.5A1.5 1.5 0 017.5 3h9A1.5 1.5 0 0118 4.5V6m-6 4.5v6m-3-6v6m6-6v6"
                         />
-                        <button onClick={addTask} className="ml-2 text-blue-500 hover:text-blue-700">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                            </svg>
-                        </button>
-                    </div>
+                      </svg>
+                    </button>
+                  </div>
                 </div>
-            </PopoverContent>
-        </Popover>
-    );
+              ))}
+          </div>
+          <div className="flex items-center mt-4">
+            <Input
+              value={newTask}
+              onChange={(e) => setNewTask(e.target.value)}
+              placeholder="I want..."
+              className="bg-gray-800 border border-gray-600 rounded-md text-white p-2 w-full"
+            />
+            <button
+              onClick={addTask}
+              className="ml-2 text-blue-500 hover:text-blue-700"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 4.5v15m7.5-7.5h-15"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
 };
 
 export default TaskPopover;
